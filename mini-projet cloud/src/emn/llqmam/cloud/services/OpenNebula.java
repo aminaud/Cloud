fc9bb1867cba02ee07bc59a37cc67f15a7e7b2cc
Quentin Mahé <quentin.mahe@live.fr>
2013-11-24 15:13:57 +0100

    added xml-rpc libraries

--- mini-projet cloud/src/emn/llqmam/cloud/services/OpenNebula.java
+++ mini-projet cloud/src/emn/llqmam/cloud/services/OpenNebula.java
@@ -1,5 +1,9 @@
 package emn.llqmam.cloud.services;
 
+import org.opennebula.client.Client;
+import org.opennebula.client.OneResponse;
+import org.opennebula.client.vm.VirtualMachine;
+
 public class OpenNebula implements IOpenNebula {
 
 	/*
@@ -7,6 +11,87 @@ public class OpenNebula implements IOpenNebula {
 	 * return int (1 = connected, 0=failed)
 	 */
 	public int login(String vm) {
+            // First of all, a Client object has to be created.
+// Here the client will try to connect to OpenNebula using the default
+// options: the auth. file will be assumed to be at $ONE_AUTH, and the
+// endpoint will be set to the environment variable $ONE_XMLRPC.
+Client oneClient;
+ 
+try
+{
+    oneClient = new Client();
+ 
+    // This VM template is a valid one, but it will probably fail to run
+    // if we try to deploy it; the path for the image is unlikely to
+    // exist.
+    String vmTemplate =
+          "NAME     = vm_from_java    CPU = 0.1    MEMORY = 64\n"
+        + "DISK     = [\n"
+        + "\tsource   = \"/home/user/vmachines/ttylinux/ttylinux.img\",\n"
+        + "\ttarget   = \"hda\",\n"
+        + "\treadonly = \"no\" ]\n"
+        + "FEATURES = [ acpi=\"no\" ]";
+ 
+    System.out.print("Trying to allocate the virtual machine... ");
+    OneResponse rc = VirtualMachine.allocate(oneClient, vmTemplate);
+ 
+    if( rc.isError() )
+    {
+        System.out.println( "failed!");
+        throw new Exception( rc.getErrorMessage() );
+    }
+ 
+    // The response message is the new VM's ID
+    int newVMID = Integer.parseInt(rc.getMessage());
+    System.out.println("ok, ID " + newVMID + ".");
+ 
+    // We can create a representation for the new VM, using the returned
+    // VM-ID
+    VirtualMachine vm = new VirtualMachine(newVMID, oneClient);
+ 
+    // Let's hold the VM, so the scheduler won't try to deploy it
+    System.out.print("Trying to hold the new VM... ");
+    rc = vm.hold();
+ 
+    if(rc.isError())
+    {
+        System.out.println("failed!");
+        throw new Exception( rc.getErrorMessage() );
+    }
+ 
+    // And now we can request its information.
+    rc = vm.info();
+ 
+    if(rc.isError())
+        throw new Exception( rc.getErrorMessage() );
+ 
+    System.out.println();
+    System.out.println(
+            "This is the information OpenNebula stores for the new VM:");
+    System.out.println(rc.getMessage() + "\n");
+ 
+    // This VirtualMachine object has some helpers, so we can access its
+    // attributes easily (remember to load the data first using the info
+    // method).
+    System.out.println("The new VM " +
+            vm.getName() + " has status: " + vm.status());
+ 
+    // And we can also use xpath expressions
+    System.out.println("The path of the disk is");
+    System.out.println( "\t" + vm.xpath("template/disk/source") );
+ 
+    // We have also some useful helpers for the actions you can perform
+    // on a virtual machine, like cancel or finalize:
+ 
+    rc = vm.finalizeVM();
+    System.out.println("\nTrying to finalize (delete) the VM " +
+                        vm.getId() + "...");
+ 
+}
+catch (Exception e)
+{
+    System.out.println(e.getMessage());
+}
 		return 0;
 	}
 
