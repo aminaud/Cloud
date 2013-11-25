99686016041b3200101cddfa66e0b260435885cd
Quentin Mahé <quentin.mahe@live.fr>
2013-11-25 18:32:41 +0100

    method to retrieve all the VMs and the nodes

--- mini-projet cloud/src/emn/llqmam/cloud/Main.java
+++ mini-projet cloud/src/emn/llqmam/cloud/Main.java
@@ -2,10 +2,9 @@ package emn.llqmam.cloud;
 
 import emn.llqmam.cloud.application.ApplicationFactory;
 import emn.llqmam.cloud.application.IApplication;
+import emn.llqmam.cloud.data.Vm;
 import emn.llqmam.cloud.services.OpenNebula;
 import emn.llqmam.cloud.tools.Tools;
-
-
 
 /**
  * 
@@ -16,13 +15,19 @@ public class Main {
 
 	private static IApplication application;
 
-	public static void main( String[] args ) {
+	public static void main(String[] args) {
 
 		application = ApplicationFactory.getNewApplication();
 
 		application.start();
-		
+
 		OpenNebula on = new OpenNebula();
-		on.login("node1_1", "oneadmin", "5bd7fcf39891cdff5896e10a79b7cd9e",Tools.get_IP()+":2633");
+		Vm vm = new Vm();
+		vm = on.login("node1_1", "oneadmin",
+				"5bd7fcf39891cdff5896e10a79b7cd9e", Tools.get_IP() + ":2633");
+		System.out.println(vm.get_version() + " " + vm.get_name());
+		System.out.println(vm.checkCompatibility());
+		System.out.println(vm.retrieveVMsInfo());
+		System.out.println(vm.retrieveNodesInfo());
 	}
 }
